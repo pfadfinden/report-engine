f51512b52908957066ad9a9bd9b8420a46f809fc
juliusstoerrle <25788444+juliusstoerrle@users.noreply.github.com>
2025-06-01 22:48:27 +0200

    executor, azure-report-executor: scaffold initial modules

---
+++ executor/src/main/java/de/pfadfinden/reports_engine/executor/Exceptions/FailedToLoadReport.java
@@ -0,0 +1,8 @@
+package de.pfadfinden.reports_engine.executor.Exceptions;
+
+public class FailedToLoadReport extends Exception {
+
+   public FailedToLoadReport(Throwable cause) {
+      super(cause);
+   }
+}
